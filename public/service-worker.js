1eb638938a0328488265478ac8b8285fb6566306
Ikra Rafi <irafi@nlihc.org>
2021-03-27 14:40:57 -0400

    image sixe typo in manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,6 @@
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
@@ -6,19 +9,13 @@ const FILES_TO_CACHE = [
     "/assets/css/style.css",
     "/favicon.ico",
     "/service-worker.js",
-    "manifest.webmanifest",
-    "/assets/images/icons/icon-72x72.png",
-    "/assets/images/icons/icon-96x96.png",
-    "/assets/images/icons/icon-128x128.png",
-    "/assets/images/icons/icon-144x144.png",
-    "/assets/images/icons/icon-152x152.png",
-    "/assets/images/icons/icon-192x192.png",
-    "/assets/images/icons/icon-384x384.png",
-    "/assets/images/icons/icon-512x512.png",
+    "manifest.json",
+    "/assets/images/android-chrome-192x192.png",
+    "/assets/images/android-chrome-512x512.png",
+    "/assets/images/apple-touch-icon.png",
 ];
 
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
+
 
 // install
 self.addEventListener("install", function(evt) {
